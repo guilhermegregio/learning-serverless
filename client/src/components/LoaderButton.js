515227b94c2d7a246f5aaf5bfb0ab93d421d2386
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2017-09-10 16:48:28 +0000

    Added styled component on button loader

--- client/src/components/LoaderButton.js
+++ client/src/components/LoaderButton.js
@@ -1,20 +1,27 @@
 import React from "react";
 import { Button, Glyphicon } from "react-bootstrap";
-import "./LoaderButton.css";
+import styled, { keyframes } from 'styled-components'
 
-export default ({
-  isLoading,
-  text,
-  loadingText,
-  className = "",
-  disabled = false,
-  ...props
-}) =>
+const rotate360 = keyframes`
+  from { transform: scale(1) rotate(0deg); }
+  to { transform: scale(1) rotate(360deg); }
+`;
+
+const GlyphiconSpinning = styled(Glyphicon)`
+  margin-right: 7px;
+  top: 2px;
+  animation: ${rotate360} 1s infinite linear;
+`
+
+const LoaderButton = ({ isLoading, text, loadingText, className = "", disabled = false, ...props }) => (
   <Button
     className={`LoaderButton ${className}`}
     disabled={disabled || isLoading}
     {...props}
   >
-    {isLoading && <Glyphicon glyph="refresh" className="spinning" />}
+    {isLoading && <GlyphiconSpinning glyph="refresh" className="spinning" />}
     {!isLoading ? text : loadingText}
-  </Button>;
+  </Button>
+);
+
+export default LoaderButton;
